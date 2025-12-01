32c3a5ffd4a5c9f0efcd326f46e9dfb2a1d39b7b
Pandu Rendradjaja <218643070+pandu-apsis@users.noreply.github.com>
2025-11-30 21:37:18 -0800

    Clean up day 1

--- 01rb/a.rb
+++ 01rb/a.rb
@@ -8,10 +8,10 @@ zeroes = 0
 
 lines.each do |line|
   line = line.rstrip
-  sign = line[0] == 'L' ? -1 : 1
+  sign = line.start_with?('L') ? -1 : 1
   n = Integer(line[1..])
   dial = (dial + sign * n) % 100
-  zeroes += 1 if dial == 0
+  zeroes += 1 if dial.zero?
 end
 
 puts zeroes
--- 01rb/b.rb
+++ 01rb/b.rb
@@ -8,11 +8,11 @@ zeroes = 0
 
 lines.each do |line|
   line = line.rstrip
-  sign = line[0] == 'L' ? -1 : 1
+  sign = line.start_with?('L') ? -1 : 1
   n = Integer(line[1..])
-  n.times do |_|
+  n.times do
     dial = (dial + sign) % 100
-    zeroes += 1 if dial == 0
+    zeroes += 1 if dial.zero?
   end
 end
 
