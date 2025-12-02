d945c1c34a3ba4e5ccba55aa53686b2fb62a5af1
Pandu Rendradjaja <218643070+pandu-apsis@users.noreply.github.com>
2025-12-01 19:43:20 -0800

    Add check-answers.py

---
+++ check-answers.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+import sys
+import subprocess
+
+assert len(sys.argv) == 3
+
+_, p_flag, command = sys.argv
+
+assert p_flag in ['-p1', '-p2']
+part_number = int(p_flag[-1])
+
+answers = open('answers.txt').read().splitlines()
+expected_answer = answers[part_number - 1]
+
+output_lines = (
+    subprocess.check_output(command, shell=True)
+    .decode('utf-8')
+    .splitlines()
+)
+actual_answer = (
+    ([None] + output_lines)[-1]
+)
+
+if actual_answer != expected_answer:
+    print(f'Failed: {command}')
+    print(f'  - Last line of output: {actual_answer}')
+    print(f'  - Expected:            {expected_answer}')
